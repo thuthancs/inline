42103921f6c4c4790250e2ee5e6f8270fc39375f
Thu Than <than@uni.minerva.edu>
2026-01-22 17:29:28 -0800

    feat: added create page, add comment and save endpoint

--- inline-server/server.ts
+++ inline-server/server.ts
@@ -29,21 +29,25 @@ app.get("/", (_req, res) => {
 // Notion SDK
 const notion = new Client({ auth: process.env.NOTION_KEY });
 
+/* NOTION API ENDPOINTS 
+    - Search pages, databases, and data sources
+    - Create a child page in a parent page
+    - Append a block to a page
+    - Add a comment to a block
+*/
 
 // Search Notion and return titles
 app.post("/search", async (req, res) => {
     try {
         const query = String(req.body?.query).trim();
-        if (!query) return res.status(400).json({ error: "Query is required" });
-
         const response = await notion.search({ query });
 
         const results = await Promise.all(
             response.results.map(async (item: any) => {
                 if (item.object === "page") {
-                    // Pages need a retrieve to get reliable titles
                     const page = await notion.pages.retrieve({ page_id: item.id });
                     console.log(page)
+
                     return {
                         type: "page",
                         title: getTitle(page),
@@ -52,9 +56,7 @@ app.post("/search", async (req, res) => {
                 }
 
                 if (item.object === "data_source") {
-                    // Search returns a data_source object with `title`
                     return {
-                        id: item.id,
                         type: "data_source",
                         title: getTitle(item),
                         url: item.url ?? null,
@@ -62,7 +64,6 @@ app.post("/search", async (req, res) => {
                 }
 
                 if (item.object === "database") {
-                    // Search returns a database object with `title`
                     return {
                         type: "database",
                         title: getTitle(item),
@@ -79,21 +80,106 @@ app.post("/search", async (req, res) => {
             })
         );
         console.log(results)
-        res.json(results);
+        return res.json(results);
     } catch (e) {
         console.error(e);
         res.status(500).json({ error: "Notion search failed" });
     }
 });
 
+// TODO: Create a child page in a parent page/datasource
+app.post("/create-page", async (req, res) => {
+    try {
+        const parent_id = String(req.body?.parent_id).trim();
+        const title = String(req.body?.title).trim();
+        const response = await notion.pages.create({
+            parent: {
+                type: "page_id",
+                page_id: parent_id,
+            },
+            properties: {
+                Name: {
+                    title: [
+                        {
+                            type: "text",
+                            text: {
+                                content: title,
+                            }
+                        }
+                    ]
+                }
+            }
+        })
+
+        return res.json(response); // the response should containt the new page id
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({ error: "Failed to create page" });
+    }
+})
+
+// TODO: Append a block to a page
+app.patch("/save", async (req, res) => {
+    try {
+        const page_id = String(req.body?.page_id).trim();
+        const content = String(req.body?.content).trim();
+
+        const response = await notion.blocks.children.append({
+            block_id: page_id,
+            children: [
+                {
+                    object: "block",
+                    type: "paragraph",
+                    paragraph: {
+                        text: [
+                            {
+                                type: "text",
+                                text: {
+                                    content: content,
+                                },
+                            },
+                        ],
+                    },
+                },
+            ],
+        });
+
+        return res.json(response);
+    } catch (e) {
+
+    }
+})
+
+// TODO: Add a comment to a block
+app.post("/comment", async (req, res) => {
+    try {
+        const block_id = String(req.body?.block_id).trim();
+        const comment_text = String(req.body?.comment_text).trim();
+
+        const response = await notion.comments.create({
+            parent: {
+                type: "block_id",
+                block_id: block_id,
+            },
+            rich_text: [
+                {
+                    type: "text",
+                    text: {
+                        content: comment_text,
+                    },
+                },
+            ],
+        });
+
+        return res.json(response);
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({ error: "Failed to add comment" });
+    }
+})
+
+// TODO: Add an endpoint to answer user question about a specific text
+
 app.listen(PORT, () => {
     console.log(`Your app is listening on port ${PORT}`);
 });
-
-// TODO: Create a child page in a parent page
-
-// TODO: Append a block to a page
-
-// TODO: 
-
-// TODO: 
